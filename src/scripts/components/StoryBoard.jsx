8d433d42769fb4d4ddfb0f903ec2f564ff0cd88f
Alexei Guevara <alexguev@gmail.com>
2014-07-31 00:44:34 -0400

    basic displaying of stories as panel / got grid going

--- src/scripts/components/StoryBoard.jsx
+++ src/scripts/components/StoryBoard.jsx
@@ -4,22 +4,69 @@
 
 'use strict';
 
+var _ = require("underscore/underscore.js");
+
 var React = require('react');
 
 var ReactBoostrap = require('react-bootstrap'),
-    Alert = ReactBoostrap.Alert,
-    Button = ReactBoostrap.Button;
+    Panel = ReactBoostrap.Panel,
+    Grid = ReactBoostrap.Grid,
+    Row = ReactBoostrap.Row,
+    Col = ReactBoostrap.Col;
 
-var StoryBoard = React.createClass({
+require("bootstrap/less/bootstrap.less");
+
+var Story = React.createClass({
+
   render: function() {
     return (
-      <div className='main'>
-        <Button bsStyle="primary">Primary1</Button>
-        <Button bsStyle="primary">Primary2</Button>
-        <Alert bsStyle="success">
-          <strong>Holy guacamoles!</strong> Best check yo self, you're not looking too good.
-        </Alert>
-      </div>
+      <Panel header={this.props.jiraId} bsStyle="primary">
+        <Row>
+          <Col md={12}>{this.props.title}</Col>
+          <Col md={4}>Bugs</Col>
+          <Col md={4}>Sonar</Col>
+          <Col md={4}>Code</Col>
+        </Row>
+      </Panel>
+    );
+  }
+});
+
+
+var StoryBoard = React.createClass({
+
+  render: function() {
+    var storyNodes = _.map(this.props.stories, function (story) {
+      return (
+        <Story jiraId={story.jiraId} title={story.title} bugs={story.bugs} sonar={story.sonar} code={story.code}/>
+      );
+    });
+
+    var groups = _.reduce(storyNodes, function (result, story) {
+      if (_.last(result).length < 3) {
+        _.last(result).push(story)
+      } else {
+        result.push([story]);
+      }
+      return result;
+    }, [[]]);
+
+    return (
+        <Grid>
+          {_.map(groups, function (group) {
+            return (
+                <Row>
+                  {_.map(group, function (story) {
+                    return (
+                      <Col key={story.jiraId} md={4}>
+                        {story}
+                      </Col>
+                    );
+                  })}
+                </Row>
+            );
+          })}
+        </Grid>
     );
   }
 });
